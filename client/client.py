d32a3a37231cfffd0c406c94b28241e4cb74de94
huy <nguyenduchuy1711@gmail.com>
2023-08-16 21:54:19 +0700

    add language model to client inference

---
+++ client/client.py
@@ -0,0 +1,59 @@
+import os
+
+import kenlm
+import numpy as np
+import soundfile as sf
+import tritonclient.http as httpclient
+from tritonclient.utils import triton_to_np_dtype
+from transformers import Wav2Vec2Processor
+from pyctcdecode import Alphabet, BeamSearchDecoderCTC, LanguageModel
+
+
+LM_DIR = "../checkpoints/"
+
+def get_decoder_ngram_model(tokenizer, ngram_lm_path):
+    vocab_dict = tokenizer.get_vocab()
+    sort_vocab = sorted((value, key) for (key, value) in vocab_dict.items())
+    vocab = [x[1] for x in sort_vocab][:-2]
+    vocab_list = vocab
+    # convert ctc blank character representation
+    vocab_list[tokenizer.pad_token_id] = ""
+    # replace special characters
+    vocab_list[tokenizer.unk_token_id] = ""
+    # convert space character representation
+    vocab_list[tokenizer.word_delimiter_token_id] = " "
+    # specify ctc blank char index, since conventially it is the last entry of the logit matrix
+    alphabet = Alphabet.build_alphabet(vocab_list, ctc_token_idx=tokenizer.pad_token_id)
+    lm_model = kenlm.Model(ngram_lm_path)
+    decoder = BeamSearchDecoderCTC(alphabet,
+                                   language_model=LanguageModel(lm_model))
+    return decoder
+
+def preprocess(file, processor):
+  speech_array, sr = sf.read(file)
+  features = processor(speech_array, sampling_rate=16000, return_tensors="pt")
+  return features.input_values.numpy()
+
+if __name__ == "__main__":
+
+  processor = Wav2Vec2Processor.from_pretrained("../checkpoints")
+  lm_file = os.path.join(LM_DIR, "lm_4.arpa")
+  ngram_lm_model = get_decoder_ngram_model(processor.tokenizer, lm_file)
+  audio_path = "../data/audio/0a1d75c4-f578-4042-9e6b-b550f7a95d3a.wav"
+  input_audio = preprocess(audio_path, processor)
+
+  # Setting up client
+  client = httpclient.InferenceServerClient(url="34.124.237.129:8000")
+
+  inputs = httpclient.InferInput("input", input_audio.shape, datatype="FP32")
+  inputs.set_data_from_numpy(input_audio)
+
+  outputs = httpclient.InferRequestedOutput("output")
+
+  # Querying the server
+  results = client.infer(model_name="wav2vec", inputs=[inputs], outputs=[outputs])
+  inference_output = results.as_numpy('output')
+  inference_output = np.squeeze(inference_output.astype(np.float32))
+  
+  prediction = ngram_lm_model.decode(inference_output, beam_width=500)
+  print(prediction)
--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 import torch.nn as nn
-# import kenlm
-# from pyctcdecode import Alphabet, BeamSearchDecoderCTC, LanguageModel
 
 def deleteEncodingLayers(model, num_layers_to_keep):
     oldModuleList = model.wav2vec2.encoder.layers
@@ -11,22 +9,3 @@ def deleteEncodingLayers(model, num_layers_to_keep):
 
     model.wav2vec2.encoder.layers = newModuleList
     return model
-
-
-# def get_decoder_ngram_model(tokenizer, ngram_lm_path):
-#     vocab_dict = tokenizer.get_vocab()
-#     sort_vocab = sorted((value, key) for (key, value) in vocab_dict.items())
-#     vocab = [x[1] for x in sort_vocab][:-2]
-#     vocab_list = vocab
-#     # convert ctc blank character representation
-#     vocab_list[tokenizer.pad_token_id] = ""
-#     # replace special characters
-#     vocab_list[tokenizer.unk_token_id] = ""
-#     # convert space character representation
-#     vocab_list[tokenizer.word_delimiter_token_id] = " "
-#     # specify ctc blank char index, since conventially it is the last entry of the logit matrix
-#     alphabet = Alphabet.build_alphabet(vocab_list, ctc_token_idx=tokenizer.pad_token_id)
-#     lm_model = kenlm.Model(ngram_lm_path)
-#     decoder = BeamSearchDecoderCTC(alphabet,
-#                                    language_model=LanguageModel(lm_model))
-#     return decoder
